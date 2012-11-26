7ca703859bd448dfdb1545d5db4a020e95479ea5
Viator <viator@via-net.org>
2012-11-27 02:34:25 +0400

    recurse fix

--- vkagrab.py
+++ vkagrab.py
@@ -11,6 +11,9 @@ from grab import Grab
 from grab import error as grab_errors
 import re
 from time import sleep
+import sys
+
+sys.setrecursionlimit(100000)
 
 try:
     import config
@@ -48,6 +51,7 @@ class VKG(Grab):
         Grab.__init__(self)
         if log_dir is not None:
             self.setup(log_dir = log_dir)
+        self.open_cache = False
         self.setup(hammer_mode=True, hammer_timeouts=((2, 5), (10, 15), (20, 30)))
         self.auth()
 
@@ -78,7 +82,7 @@ class VKG(Grab):
         try:
             actions = self.xpath_list('//*[@class="actions"]')[0].xpath('li')
         except IndexError:
-            sleep(2)
+            sleep(4)
             return self.get_photo(name, start, uri, inc)
 
         full_size_url = actions[-1:][0].xpath('a')[0].get('href')
@@ -116,45 +120,80 @@ class VKG(Grab):
     def normalize(self, name):
         return ''.join([self.test_symbol(symbol) for symbol in name])
 
-    def get_photos(self, name, uri):
+    def get_album(self, name, uri, try_count = 0):
+        if try_count == 5:
+            return
         print 'Get album: ', name
         self.go_vk(uri[1:])
         try:
             photos = self.xpath_list('//*[@class="al_photo"]')
+            uri = photos[0].get('href')
         except IndexError:
+            sleep(4)
+            try_count += 1
+            self.get_album(name, uri, try_count)
             return
-        uri = photos[0].get('href')
         self.get_photo(name, uri, uri, 1)
 
-    def get_albums(self, id, offset = 0, variant = 0):
+    def get_albums(self, id, variant = 0, offset = 0, cache_e = 0):
         print 'Get albums: ', id, offset
+
         if variant == self.GROUP:
             self.go_vk('albums-{}?offset={}'.format(id, offset))
         elif variant == self.USER:
             self.go_vk('albums{}?offset={}'.format(id, offset))
-        offset += 20
+
         albums = self.xpath_list('//*[@class="album"]')
+
         if albums == []:
             return 0
-        for album in albums:
+
+        for e, album in enumerate(albums):
+            if e < cache_e:
+                continue
             uri = album.xpath('a')[0].get('href')
             name = self.normalize(album.find_class('name')[0].text_content())
             try:
                 os.mkdir(self.dest_dir.format(name))
             except OSError:
                 pass
+            self.save_cache(offset, e)
+            self.get_album(name, uri)
 
-            self.get_photos(name, uri)
-        return self.get_albums(id, offset, variant)
+        offset += 20
+        return self.get_albums(id, variant, offset)
+
+    def save_cache(self, offset, album):
+        open('cache', 'w').write('{}:{}'.format(offset, album))
+
+    def load_cache(self):
+        if not self.open_cache:
+            self.open_cache = True
+            try:
+                return map(lambda x: int(x), open('cache', 'r').read().split(':'))
+            except IOError:
+                return [0, 0]
+        else:
+            return [0, 0]
+
+    def clear_cache(self):
+        try:
+            os.remove('cache')
+        except OSError:
+            pass
 
     def start(self):
         self.go_vk(self.grab_uid)
+        offset, cache_e = self.load_cache()
+        print offset, cache_e
         if self.response.code == 200:
             st = self.response.unicode_body()
             try:
-                return self.get_albums(re.search(r'\"\/photos-(\d+)\"', st).group(1), 0, self.GROUP)
+                return self.get_albums(re.search(r'\"\/photos-(\d+)\"', st)\
+                    .group(1), self.GROUP, offset, cache_e)
             except AttributeError:
-                return self.get_albums(re.search(r'\"\/photos(\d+)\"', st).group(1), 0, self.USER)
+                return self.get_albums(re.search(r'\"\/photos(\d+)\"', st)\
+                    .group(1), self.USER, offset, cache_e)
         else:
             return 1
 
@@ -181,6 +220,7 @@ def main():
         vkg = VKG(uid, dest_dir = dest_dir,
                     login = LOGIN, password = PASSWORD)
         vkg.start()
+        vkg.clear_cache()
 
 if __name__ == '__main__':
     #test()
